f38d80074fcb367a205a19210b6daa4cb05dc1c0
Paul Williamsen <pwilliamsen@me.com>
2019-01-24 08:51:31 -0800

    remove hardcoded database

--- schema.sql
+++ schema.sql
@@ -11,8 +11,3 @@ CREATE TABLE saved (
   description TEXT,
   bookshelf VARCHAR(255)
 );
-
-INSERT INTO saved (title, img_url, authors, isbn, description, bookshelf) 
-VALUES('turtle','www.google.com','Paul Williamsen','ISBN: 1728374526371','This book is amazing.','Rusty'),
-('potato','www.google.com','Chris Ball','ISBN: 1728378726371','This book is terrible.','Short'),
-('The Best On The Market','www.google.com','Paul','ISBN: 8888874526371','Such good.','Rusty');
